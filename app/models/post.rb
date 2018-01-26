a61b55b34be6ae1c88b4aef32c69ec49987798b6
Jacqueline Lee <anginblu@gmail.com>
2018-01-26 19:46:50 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", "Guess"] }
+  validates :title, presence: true, inclusion: { in: ["Won't Believe", "Secret", "Top", "Guess"] }
   validates :content, presence: true, length: { :minimum => 250 }
   validates :summary, presence: true, length: { :maximum => 250 }
   validates :category, presence: true, inclusion: { in: ["Fiction", "Non-Fiction"] }
